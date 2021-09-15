c1439f25138d9008f89659bd2bdc9267784aaeeb
chao <yycrainbow@icloud.com>
2021-09-15 21:52:15 +0800

    init app

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    style:{
+        postcss:{
+            plugin: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
+}
